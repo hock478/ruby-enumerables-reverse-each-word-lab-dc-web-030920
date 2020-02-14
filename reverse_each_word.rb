87d1ea58f470ef7eaea71c3070cdb040ff4e206c
Dawit Gizaw <gizawdawit123@gmail.com>
2020-02-14 00:13:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ def reverse_each_word(sentence)
   use = []
   use = sentence.split(" ")
   reverse = ""
-  use.each { |n| reverse += " " + n.reverse + " "}
+  use.each { |n| reverse +=  n.reverse + " "}
   reverse
 end
